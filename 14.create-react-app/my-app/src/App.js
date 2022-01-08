d2db253c515c15c229e10e25f253e05af26266d3
yusufstawan <yusufsetiyawan45@gmail.com>
2022-01-08 17:08:40 +0700

    add list-transaction

--- 14.create-react-app/my-app/src/App.js
+++ 14.create-react-app/my-app/src/App.js
@@ -1,34 +1,36 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Transaction from './components/Transaction';
 
-const initTransactions = [
+const initTansactions = [
     {
-        "id": "619941345654",
+        "id": "619941539079",
         "tanggal": new Date("01 Nov 2021 9:30").getTime(),
         "keterangan": "Gaji bulanan",
-        "nominal": 2500000
+        "nominal": 2500000,
     },
     {
-        "id": "741941345654",
-        "tanggal": new Date("23 Nov 2021 10:30").getTime(),
-        "keterangan": "Uang Lembur",
-        "nominal": 75000
+        "id": "749179155708",
+        "tanggal": new Date("23 Nov 2021 10:00").getTime(),
+        "keterangan": "Uang lembur ",
+        "nominal": 750000,
     },
     {
-        "id": "568941345654",
-        "tanggal": new Date("24 Sept 2021 10:00").getTime(),
-        "keterangan": "Gaji bulanan",
-        "nominal": -250000
+        "id": "568004092688",
+        "tanggal": new Date("24 Sept 2021 10:30").getTime(),
+        "keterangan": "Beli sepatu",
+        "nominal": -150000,
     }
-]
+];
 
 const App = () => {
-    const [transactions, setTransaction] = useState(initTransactions);
-    console.log(transactions)
+    const [transactions, setTransaction] = useState(initTansactions);
+
     return (
         <React.Fragment>
             <Header />
+            <Transaction transactions={transactions} />
             <Footer />
         </React.Fragment>
     )
---
+++ 14.create-react-app/my-app/src/components/ListTransaction.js
@@ -0,0 +1,31 @@
+const ListTransaction = (props) => {
+
+    // Atur tanggal untuk tampilan
+    const getTanggal = (tanggal) => {
+        let dateObj = new Date(tanggal);
+
+        const arrBulan = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ags", "Sep", "Okt", "Nov", "Des"];
+
+        let bulan = arrBulan[dateObj.getMonth()];
+        return `${dateObj.getDate()} ${bulan} ${dateObj.getFullYear()}`;
+    }
+
+    // untuk menentukan style list transaction (warna border kiri)
+    let classTransaction = (props.nominal > 0) ?
+        "list-pemasukan" : "list-pengeluaran";
+
+    return (
+        <div className={`list-transaction p-1 mb-2 ${classTransaction}`}>
+            <div className="ms-2 d-flex flex-column">
+                <p className="m-0 fs-5">{props.keterangan}</p>
+                <small>{getTanggal(props.tanggal)}</small>
+            </div>
+            <p className="fs-5 mb-1 me-3 text">
+                Rp. {props.nominal.toLocaleString('id-ID')}
+            </p>
+            <span className="delete-icon">x</span>
+        </div>
+    )
+}
+
+export default ListTransaction;
